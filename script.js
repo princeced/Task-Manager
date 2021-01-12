0e63d124d13cd2d10885f69b7af75b013a478273
princeced <prince.singh15136@gmail.com>
2021-01-12 21:04:27 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+//let index = 0;
+
+/**function addTasks() {
+    let adtasks = $('#inp_task').val();
+
+
+    let list = $("li").attr({
+        class: "listItems",
+    });
+    let checkboxs = $("input").attr({
+        type: "checkbox",
+        id: "inputCheck",
+
+    });
+    let labels = $("label").attr({
+        id: "inputLabel",
+    });
+    let editInput = $("input").attr({
+        type: "Text",
+        id: "editTextbox"
+    });
+
+    let editButton = $().attr({
+        id: "editButton",
+        onclick: editThis(this),
+        value: "Edit",
+    });
+    let deleteButton = $("button").attr({
+        id: "deletebtn",
+        onclick: deleteThis(this),
+        Value: "Delete"
+    });
+
+    $("ul").append(list, checkboxs, labels, editInput, editButton, deleteButton);
+
+
+}**/
